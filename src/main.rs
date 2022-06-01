b811f01ba3494e5f302c6d1b54aed542ca58493c
Andrew Burkhart <Burkhartfc@iCloud.com>
2022-05-31 19:41:30 -0700

    Add render function

--- src/main.rs
+++ src/main.rs
@@ -110,6 +110,35 @@ fn test_pixel_to_point() {
     )
 }
 
+/// Render a rectangle of the Mandelbrot set into a
+/// buffer of pixels
+///
+/// The `bounds` argument gives the width and height
+/// of the buffer `pixels`, which holds one grayscale
+/// pixel per byte
+/// The `upper_left` and `upper_right` arguments
+/// specify points on the complex plane corresponding
+/// to the upper-left and lower-right corners of the
+/// pixel buffer
+fn render(
+    pixel: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let points = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => None,
+                Some(count) => 255 - count as u8,
+            }
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
