56bc7d9cb999ef71ff19111af3bebd72c0cfd26d
Andrew Burkhart <Burkhartfc@iCloud.com>
2022-05-31 19:39:07 -0700

    Add parse_pair function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 /// Try to determine if `c` is in the
 /// mandelbrot set usin at most `limit`
 /// iterations to decide
@@ -17,6 +19,30 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     }
 
     None
+}
+
+/// Parse the given string as a coordinate pair
+///
+/// If `s` does not parse, return None
+/// Otherwise, return `Some<(x, y)>`
+fn parse_pair<T: FromStr>(s: &str, seperator: char) -> Option<(T, T)> {
+    match s.find(seperator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<i32>("0.5x1.5", 'x'), None);
 }
 
 fn main() {
