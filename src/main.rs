b77da9b6eb9655765841a80e213ff627b5ea685f
Andrew Burkhart <Burkhartfc@iCloud.com>
2022-05-31 19:42:03 -0700

    Add write_image function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use image::png::PNGEncoder;
+use image::ColorType;
 use num::Complex;
+use std::fs::File;
 use std::str::FromStr;
 
 /// Try to determine if `c` is in the
@@ -137,6 +140,21 @@ fn render(
             }
         }
     }
+}
+
+/// Write the buffer `pixels`, whose dimensions are
+/// given by `bounds`, to the file named `filename`
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    let output = File::create(&filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+
+    Ok(())
 }
 
 fn main() {
