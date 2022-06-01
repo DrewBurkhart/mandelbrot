dfc2273d9231816d28a3c803f19219b37eaf04a7
Andrew Burkhart <Burkhartfc@iCloud.com>
2022-05-31 19:38:08 -0700

    Add mandelbrot function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+/// Try to determine if `c` is in the
+/// mandelbrot set usin at most `limit`
+/// iterations to decide
+///
+/// If `c` is not a member, return None
+/// Otherwise, return Some(i) where i is the
+/// number of iterations it took for c to
+/// leave the circle of radius 2 centered
+/// on the origin.
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+
+    None
+}
+
 fn main() {
     println!("Hello, world!");
 }
